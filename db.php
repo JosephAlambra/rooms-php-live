66da71fde2ca3e7c8c0e35e771491ca4cdf66641
Joseph Alambra <josephnathanielalambra@gmail.com>
2025-03-25 03:03:42 +0800

    fixxx

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "bzgf4ndakhr5jch691lv-mysql.services.clever-cloud.com";
-$username = "u0xcngdlpa9p7jby";
-$password = "W4yKKXsdBUsciNBTUqNX"; // replace this!
-$dbname = "bzgf4ndakhr5jch691lv";
+// Use environment variables for database connection
+$host = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
