b23e8c1c139e7fc4d39bec65d75a4dc55e5740f1
Joseph Alambra <josephnathanielalambra@gmail.com>
2025-03-25 08:45:05 +0800

    replaced 309 with 306

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 <body>
   <h1>Welcome to ROOMS Status Portal</h1>
   <ul>
+
+    <li><a href="GV306.php">View GV 306</a></li>
     <li><a href="GV307.php">View GV 307</a></li>
     <li><a href="GV308.php">View GV 308</a></li>
-    <li><a href="GV309.php">View GV 309</a></li>
+    
   </ul>
 </body>
 </html>
